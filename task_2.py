fdf25823c14f738b6862390d26ffcc294069dbd5
Denis S <dskvortsov79@yandex.ru>
2025-04-24 21:43:27 +0300

    resolved issue mentioned in the comment

--- task_2.py
+++ task_2.py
@@ -1,11 +1,10 @@
 class Tester:
-    deadline = True
-    name = ''
+
     def __init__(self, name):
         self.name = name
-        
+        self.deadline = True
 
-    def work_hard(self, deadline = True):
+    def work_hard(self, deadline=True):
         self.deadline = deadline
         if self.deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
@@ -15,4 +14,4 @@ class Tester:
 tester_1 = Tester(name='tester_1')
 tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!' 
